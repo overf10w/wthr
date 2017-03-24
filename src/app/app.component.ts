259aa8c57739f168cd4de3bfadddf960b29162b4
pvvner <creedge@yandex.ru>
2017-03-24 02:25:25 +0200

    Super basic setup. 3 cities displayed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: './app.component.html'
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent  {
+  title = 'Best weather APP EVER';
+  cities: City[] = [
+    { id: 1, name: 'Kiev' },
+    { id: 2, name: 'London'},
+    { id: 3, name: 'New York'}
+  ];
+}
+
+export class City {
+  id: number;
+  name: string;
+}
