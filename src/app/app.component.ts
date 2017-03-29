d40ab89ad0c0057e8a15317c59c8e3246ecf1c0d
pvvner <creedge@yandex.ru>
2017-03-30 01:18:50 +0300

    bit of refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,20 +23,10 @@ export class AppComponent  {
 
   onSelect(city: City): void {
     this.selectedCity = city;
-    // console.log(this.selectedCity.name + ' selected');
     this.weatherService.getWeather(this.selectedCity.latitude, this.selectedCity.longitude)
                        .subscribe(
                          data => {
-                           // console.log(data.dailySummary);
-
-                           // this.selectedForecast = data;
-                           // console.log(this.selectedForecast.dailySummary);
-
                            this.selectedForecast = data;
-                           // console.log(this.selectedForecast.dailySummary);
-                           // console.log(this.selectedForecast.hourlySummary);
-
-                           // console.log(this.forecast.dailySummary);
                          }, error => this.errorMsg = <any>error);
   }
 }
